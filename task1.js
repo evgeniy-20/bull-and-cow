7ba04dfe06a174b6c7c5d61f2ba89f0dad667831
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2019-04-03 20:36:59 +0300

    bull-and-cow

---
+++ task1.js
@@ -0,0 +1,6 @@
+var numberFour = [];
+numberFour.push(Math.floor(Math.random() * 9));
+numberFour.push(Math.floor(Math.random() * 9));
+numberFour.push(Math.floor(Math.random() * 9));
+numberFour.push(Math.floor(Math.random() * 9));
+console.log(numberFour.length);
