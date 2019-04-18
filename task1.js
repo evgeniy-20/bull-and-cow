308a6bf2ed233fc37b4f5ecaf3fe9eba1c604a76
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2019-04-18 05:35:02 +0300

    bull-and-cow

--- task1.js
+++ task1.js
@@ -1,8 +1,6 @@
 var numberOne = Math.floor(Math.random() * 9);
-console.log(numberOne);
-var numberFour = [];
-numberFour.push(numberOne);
-numberFour.push(numberOne);
-numberFour.push(numberOne);
-numberFour.push(numberOne);
-console.log(numberFour, numberOne);
+var numberTwo = Math.floor(Math.random() * 9);
+var numberThree = Math.floor(Math.random() * 9);
+var numberFour = Math.floor(Math.random() * 9);
+var numbers = [numberOne, numberTwo, numberThree, numberFour];
+console.log(numbers);
