7e9108a3079930c07e184f6140284b640400b3ee
evgeniy20 <evgeniy.prokopiv@gmail.com>
2019-07-04 16:49:01 +0300

    indexOf

--- task2.js
+++
@@ -1,5 +0,0 @@
-var numbers = [1, 2, 5, 9];
-var numbers2 = [16, 44];
-var numbers3 = numbers.concat(numbers2);
-var result = numbers3.join("|");
-console.log(result);
---
+++ test/task5.js
@@ -0,0 +1,4 @@
+var array = [1, 2, 5, 8, 13, 16];
+var random = 44;
+var result = array.indexOf(random)!== -1//тут нужно написать что-то чтобы result был false
+console.log(result);
