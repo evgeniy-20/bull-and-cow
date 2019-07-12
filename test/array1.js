6be6ac925bf7b5d7c7690b3ae7b853f1be53ccf7
evgeniy20 <evgeniy.prokopiv@gmail.com>
2019-07-12 17:34:55 +0300

    function1

--- test/array1.js
+++ test/array1.js
@@ -1,6 +1,6 @@
-function tellFortune(children,partner,city,Jobtitle) {
-    console.log("You will be a " + Jobtitle + " in " + city + " and" + " married to " + partner + " with " + children + " kids.");
+function tellFortune(children, partner, city, Jobtitle) {
+    console.log("You will be a " + Jobtitle + " in " + city + " and " + " married to " + partner + " with " + children + " kids.");
 }
-tellFortune("toy", "Urkraina", "car");
-tellFortune( "London", "Korea", "Fortune teller");
-tellFortune( "London", "Korea", "Fortune teller");
+tellFortune(5, "Victiria", "Kiyv", "student");
+tellFortune(10, "Ira", "Odessa", "programmer");
+tellFortune(15, "Lyudmila", "Vinnitsa", "banker");
