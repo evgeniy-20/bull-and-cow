df9c0f6d15b81396ba66544dbd092a05c8cfa3bf
evgeniy20 <evgeniy.prokopiv@gmail.com>
2019-08-06 12:23:52 +0300

    list-2

--- cat/myList.js
+++ cat/myList.js
@@ -1,1 +1,5 @@
-$('.list').append("<li>test</li>");
+var toDoList = ["learn english"," to do programming"," feed chicks"," wrest grass"];
+for (var i = 0; i <= toDoList.length; i++) {
+    $( ".list" ).append( "<li>" + toDoList[i] + "</li>");
+}
+
