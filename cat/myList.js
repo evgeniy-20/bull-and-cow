7f46270333cf60654caa7ab4e31f7496855637ee
evgeniy20 <evgeniy.prokopiv@gmail.com>
2019-08-09 20:09:37 +0300

    List

--- cat/myList.js
+++ cat/myList.js
@@ -1,5 +1,10 @@
 var toDoList = ["learn english"," to do programming"," feed chicks"," wrest grass"];
-for (var i = 0; i <= toDoList.length; i++) {
+for (var i = 0; i < toDoList.length; i++) {
     $( ".list" ).append( "<li>" + toDoList[i] + "</li>");
 }
-
+var headingElement = document.getElementById("guess-button");
+console.log(headingElement.innerHTML);
+function clickHandler() {
+    prompt("Please enter new task name");
+}
+$("button").click(clickHandler);
