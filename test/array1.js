8ca69be1bfca62810fbda2e58f2b4162628a0065
evgeniy20 <evgeniy.prokopiv@gmail.com>
2019-08-20 17:33:06 +0300

    starbuzz

--- test/array1.js
+++
@@ -1,6 +0,0 @@
-function tellFortune(children, partner, city, Jobtitle) {
-    console.log("You will be a " + Jobtitle + " in " + city + " and " + " married to " + partner + " with " + children + " kids.");
-}
-tellFortune(5, "Victiria", "Kiyv", "student");
-tellFortune(10, "Ira", "Odessa", "programmer");
-tellFortune(15, "Lyudmila", "Vinnitsa", "banker");
--- test/arry.js
+++
@@ -1,6 +0,0 @@
-var children = 3;
-var partner ='Victoria';
-var city ='Kiev';
-var Jobtitle ='student';
-var fortune = 'You will be a ' + Jobtitle + ' in ' + city + ' and' + ' married to ' + partner + ' with ' + children + ' kids.';
-console.log(fortune);
